a6b9527f459358dff081a1057d831d5db8cf7ab9
shubham sharma <shubham.com321@gmail.com>
2025-01-09 14:32:12 +0545

    The jwt authentication routes and handles

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,11 @@
 ﻿from django.urls import path
 from .views import UserView, ComplaintView, UserComplaintView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('', UserView.as_view()),
     path('complaint/', ComplaintView.as_view()),
     path('complaint/user/', UserComplaintView.as_view()),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
 ]
