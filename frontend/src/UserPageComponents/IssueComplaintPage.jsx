081a7552430db0b3ca4e25e30ff1373776d2e724
satkrit15 <satkrit15@gmail.com>
2025-01-11 01:54:35 +0545

    all file managed

--- frontend/src/UserPageComponents/IssueComplaintPage.jsx
+++ frontend/src/UserPageComponents/IssueComplaintPage.jsx
@@ -62,15 +62,15 @@ const IssueComplaintPage = () => {
             <form className='h-full px-8 py-4 flex flex-col gap-8 pt-12'>
               <div className='h-auto w-[40%] flex justify-between items-end'>
                 <label htmlFor="title" className='text-2xl '>Title: </label>
-                <input value={formData.title} onChange={handleFormInput} name='title' id='title' type="text" placeholder='Enter the title for the complaint' className='h-[6vh] rounded px-4 py-2 bg-theme-primary border-b-2 border-black w-[60%] focus:outline-none' />
+                <input value={formData.title} onChange={handleFormInput} name='title' id='title' type="text" placeholder='Enter the title for the complaint' className='h-[6vh] font-serif text-[14px] rounded px-2 py-1 text-s bg-theme-primary border-b-2 border-black w-[60%] focus:outline-none' />
               </div>
               <div className='h-auto w-[40%] flex justify-between items-end'>
                 <label htmlFor="description" className='text-2xl '>Description: </label>
-                <input value={formData.description} onChange={handleFormInput} name='description' id='description' type="text" placeholder='Enter the description for the complaint' className='h-[6vh] rounded px-4 py-2 bg-theme-primary border-b-2 border-black w-[60%] focus:outline-none' />
+                <input value={formData.description} onChange={handleFormInput} name='description' id='description' type="text" placeholder='Enter the description for the complaint' className='h-[6vh] font-serif rounded text-[14px] px-2 py-1 bg-theme-primary border-b-2 border-black w-[60%] focus:outline-none' />
               </div>
               <div className='h-auto w-[40%] flex justify-between items-end'>
                 <label htmlFor="location" className='text-2xl '>Location: </label>
-                <input value={formData.location} onChange={handleFormInput} name='location' id='location' type="text" placeholder='Enter the location for the complaint' className='h-[6vh] rounded px-4 py-2 bg-theme-primary border-b-2 border-black w-[60%] focus:outline-none' />
+                <input value={formData.location} onChange={handleFormInput} name='location' id='location' type="text" placeholder='Enter the location for the complaint' className='h-[6vh] font-serif text-[14px] rounded px-2 py-1 bg-theme-primary border-b-2 border-black w-[60%] focus:outline-none' />
               </div>
               <div className='h-auto w-[40%] flex justify-between items-end'>
                 <button onClick={handleFormSubmit} className={`h-full w-full text-white bg-green-500 hover:bg-green-600 py-2 px-4 rounded text-xl ${isLoading ? 'select-none pointer-events-none': ''}`}>
