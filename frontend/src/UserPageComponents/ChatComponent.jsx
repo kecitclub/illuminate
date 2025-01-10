daed6589dc4a71ea0eb3e266b16bd48566f172ff
Prabhat Baduwal <pra.baduwal3@gmail.com>
2025-01-10 11:10:23 +0545

    The chatbot finished, except UI

--- frontend/src/UserPageComponents/ChatComponent.jsx
+++ frontend/src/UserPageComponents/ChatComponent.jsx
@@ -1,26 +1,28 @@
-﻿import { faBroom } from '@fortawesome/free-solid-svg-icons'
+﻿import { text } from '@fortawesome/fontawesome-svg-core';
+import { faBroom } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import ReactMarkdown from "react-markdown";
 
 const ChatComponent = () => {
 
     const [chatLog, setChatLog] = useState([]);
     const [userInput, setUserInput] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleUserInput = (e) => {
         setUserInput(e.target.value);
     }
 
 
-    const addMessage = (text, isUser) => {
-        setChatLog((prevChatLog) => [...prevChatLog, { text, isUser }]);
-    };
-      
+    const inputRef = useRef(null);
     
     const handleKeyDown = (e) => {
         if (e.key === "Enter") {
+            setIsLoading(true);
             setChatLog((prevChatLog) => [...prevChatLog, { text: userInput, isUser: true }]);
+            console.log(chatLog)
             setUserInput("");
 
 
@@ -49,6 +51,9 @@ const ChatComponent = () => {
             
                 } catch (error) {
                   console.error("Error fetching data:", error);
+                }finally{
+                    setIsLoading(false);
+                    inputRef.current.focus();
                 }
             };
           
@@ -56,6 +61,10 @@ const ChatComponent = () => {
         }
     };
 
+    
+    useEffect(() => {
+        inputRef.current.focus();
+    });
 
     const handleClearChat = () => {
         setChatLog([]);
@@ -64,14 +73,16 @@ const ChatComponent = () => {
 
   return (
     <div className='h-full w-full bg-gray-400 flex-col'>
-        <div className='h-[90%] w-full overflow-y-scroll no-scrollbar px-6 py-4'>
+        <div className='h-[90%] w-full overflow-y-scroll no-scrollbar px-6 py-4 flex flex-col bg-gray'>
             {chatLog.map((message, index) => (
-            <h1 className={`${message.isUser ? 'text-left' : "text-right"}`} key={index}>{message}</h1>
+            <h1 className={`${message.isUser ? 'text-left self-start w-[50%] mb-12' : "text-right self-end w-[50%] mb-12"}`} key={index}>{message.isUser ? 'You:  ': "Jana Sewa(Bot):  " } <span className=' px-4 py-2 text-left'>
+                <ReactMarkdown className={`${message.isUser ? "text-left" : ''} ${message.isUser !== true && message.text.length <= 100 ? 'text-right': 'text-left'}`}>{message.text}</ReactMarkdown>
+            </span></h1>
             ))}
         </div>
         <div className='h-[10%] flex justify-start items-center gap-4'>
-            <input onKeyDown={handleKeyDown} value={userInput} onChange={handleUserInput} type="text" placeholder='Enter the prompt' className='h-full w-[90%] bg-[rgb(120,120,140)] rounded-r px-10 py-2 focus:outline-none font-normal text-xl placeholder:text-lg' />
-            <h1 onClick={handleClearChat} className='cursor-pointer hover:text-red-800'><FontAwesomeIcon icon={faBroom} className='text-2xl' />Clear Chat</h1>
+            <input ref={inputRef} onKeyDown={handleKeyDown} value={userInput} onChange={handleUserInput} type="text" placeholder={`${isLoading ? 'Loading.....': 'Enter the prompt '}`} className={`h-full w-[90%] bg-[rgb(120,120,140)] rounded-r px-10 py-2 focus:outline-none font-normal text-xl placeholder:text-lg ${isLoading ? "": ""}`} disabled={isLoading} />
+            <h1 onClick={handleClearChat} className='h-full border-t-2 border-black flex justify-center items-center cursor-pointer hover:text-red-800'><FontAwesomeIcon icon={faBroom} className='text-2xl' />Clear Chat</h1>
             
         </div>
     </div>
